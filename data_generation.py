1fb948176a8bb211aa55754a703a70fd58f02812
Anish Khazane <akkhazan@berkeley.edu>
2018-05-29 19:09:50 -0700

    added support for 3D data generation

--- data_generation.py
+++ data_generation.py
@@ -52,66 +52,50 @@ class DataGenerator(keras.utils.Sequence):
         if self.shuffle == True:
             np.random.shuffle(self.indexes)
 
-    def __data_generation(self, list_IDs_temp, list_ys_temp):
+    def __data_generation(self, list_IDs_temp, list_ys_temp, third_dimension=False):
         'Generates data containing batch_size samples' # X : (n_samples, *dim, n_channels)
         print('Generating...')
         # Initialization
         X = np.empty((self.batch_size, self.n_channels, 150, self.dim[1], self.dim[2]))
         y = np.empty((self.batch_size, self.n_channels, 150, self.dim[1], self.dim[2]))
-        #print('X shape: ', X.shape)
         # Need to split larger image into slices
-        xbatch_idx = 0
-        ybatch_idx = 0
+       
+        x_slice_idx = 0
+        y_slice_ix = 0
+        num_slices = 60
         # Generate data
         # Num Slcies
-        num_slices = 60
         for i, ID in enumerate(list_IDs_temp):
             # Load scan data for raw scan & mask
-            x_data = np.swapaxes(nib.load(ID).get_data().astype(np.float32), 0, -1)
-            y_data = np.swapaxes(nib.load(list_ys_temp[i]).get_data().astype(np.float32), 0, -1)
+            if third_dimension:
+                # set channels as first dimension for 3D Unet.
+                x_data = np.swapaxes(nib.load(ID).get_data().astype(np.float32), 0, -1)
+                y_data = np.swapaxes(nib.load(list_ys_temp[i]).get_data().astype(np.float32), 0, -1)
+                X[x_slice_idx,] = x_data
+                y[y_slice_ix,] = y_data
 
-            # if x_data.shape[0] != 150:
-            #     x_data = self.resize_image(x_data) 
-            # if y_data.shape[0] != 150:
-            #     y_data = self.resize_image(y_data) 
+                x_slice_idx += 1
+                y_slice_ix += 1
 
-            #print(x_data.shape)
+            else:
 
-            # x_data = 
-            # Iterate through slices
-             X[xbatch_idx,] = x_data
-             y[ybatch_idx,] = y_data
+                for x_idx in range(x_data.shape[2]):
+                    X[xslice_idx,] = x_data[:, :, x_idx, np.newaxis]
+                    xslice_idx+=1
+                    if xslice_idx == num_slices:
+                        break
 
-             xbatch_idx += 1
-             ybatch_idx += 1
-        #     # for x_idx in range(x_data.shape[2]):
-        #     #     X[xslice_idx,] = x_data[:, :, x_idx, np.newaxis]
-        #     #     xslice_idx+=1
-            
-        #     # for y_idx in range(y_data.shape[2]):
-        #     #     y[yslice_idx,] = y_data[:, :, y_idx, np.newaxis]
-        #     #     yslice_idx+=1
-        # # Return view of data (slice a larger array than there is info) 
-        # # bs*150,  256, 256, 1
-         x1 = X[:xbatch_idx, :, :, :].astype(np.float32)
-         y1 = y[:ybatch_idx, :, :, :].astype(np.float32)
+                
+                for y_idx in range(y_data.shape[2]):
+                    y[yslice_idx,] = y_data[:, :, y_idx, np.newaxis]
+                    yslice_idx+=1
+                    if yslice_idx == num_slices:
+                        break 
+                if xslice_idx == num_slices and yslice_idx == num_slices:
+                    break 
+        # Return view of data (slice a larger array than there is info) 
+         x1 = X[:x_slice_idx, :, :, :].astype(np.float32)
+         y1 = y[:y_slice_ix, :, :, :].astype(np.float32)
         # # print(x1.dtype)
          return x1, y1
-        #    for x_idx in range(x_data.shape[2]):
-        #        X[xslice_idx,] = x_data[:, :, x_idx, np.newaxis]
-        #        xslice_idx+=1
-        #        if xslice_idx == num_slices:
-        #            break
-            
-        #    for y_idx in range(y_data.shape[2]):
-        #        y[yslice_idx,] = y_data[:, :, y_idx, np.newaxis]
-        #        yslice_idx+=1
-        #        if yslice_idx == num_slices:
-        #            break
-        # Return view of data (slice a larger array than there is info) 
-        # bs*150,  256, 256, 1
-       # x1 = X[:xslice_idx, :, :, :].astype(np.float32)
-       # y1 = y[:yslice_idx, :, :, :].astype(np.float32)
-       # print('y1 shape : ', y1.shape)
-       # print('x1 shape : ', x1.shape)
-       # return x1, y1#keras.utils.to_categorical(y1)
+      
