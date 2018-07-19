98f537c48c09ef8873a897402a5af799f5a14ef9
mechaman <julien.hoachuck@gmail.com>
2018-07-19 02:27:01 +0000

    Update data_generator_refactored.py

--- data_generator_refactored.py
+++ data_generator_refactored.py
@@ -48,43 +48,48 @@ class DataGenerator(keras.utils.Sequence):
         min_val = np.min(x)
         norm_x = (x-min_val)/(max_val - min_val + 1e-7)
         return norm_x
-    
+
     def padImage(self, img, mask=False):
         '''
-        1) Find max dim. value and idx
-        2) Check if autre dim. == to max dim
-        3) True -> return orig. image
-        4) False -> pad dims. != argmax(dims)
-        5) Return padded image
+        Input -> img : numpy array, mask : Boolean
         ''' 
-        x_dim = img.shape[0]
-        y_dim = img.shape[1]
-        if x_dim == y_dim:
-            return img
-        else:
-            pad_w = x_dim - y_dim
-            if mask:
-                return np.pad(img, [(0,0), (pad_w//2,pad_w//2), (0,0)],
-                              mode='constant',
-                              constant_values=(1))
-            else:
-                return np.pad(img, [(0,0), (pad_w//2,pad_w//2), (0,0)],
-                              mode='constant')
+        img_c = img.copy()
+        # Get max dim
+        dims = img.shape
+        max_dim = max(dims)
+        # Padding Args.
+        pad_value = 0
+        pad_arr = [(0,0), (0,0), (0,0)]
+        # Check if mask or input to pad w. 1 or 0
+        if mask:
+            pad_value = 1
+        # Check if other dims eq. largest dim
+        for idx, dim in enumerate(dims):
+            if dim != max_dim:
+                pad_w = max_dim - dim
+                pad_arr[idx] = (pad_w//2, pad_w//2)
+                img_c = np.pad(img, pad_arr,
+                                mode='constant', constant_values=(pad_value))
+
+        return img_c
+
                 
                 
 
     def __data_generation(self, list_IDs_temp, list_ys_temp):
-        'Generates data containing batch_size samples' # X : (n_samples, *dim, n_channels)
+        'Generates data containing batch_size samples'
         # Initialization
-        X = np.empty((self.batch_size, *self.dim, self.n_channels))
-        y = np.empty((self.batch_size, *self.dim, self.n_channels))
+        X = np.empty((self.batch_size, *self.dim))
+        y = np.empty((self.batch_size, *self.dim))
 
         # Generate data
         for i, ID in enumerate(list_IDs_temp):
+            # @TODO remove when file name consistent
+            ID = ID.replace('.gz', '')
             # Store sample
-            X[i, ] = self.padImage(self.normalizeImg((nib.load(ID).get_data().astype(np.float32))[:, :, np.newaxis]))
+            X[i, ] = self.padImage(self.normalizeImg(nib.load(ID).get_data().astype(np.float32)))
             # Store sample segmentation
-            y[i, ]  = self.padImage((nib.load(list_ys_temp[i]).get_data().astype(np.float32))[:, :, np.newaxis], mask=True)
+            y[i, ]  = self.padImage((nib.load(list_ys_temp[i]).get_data().astype(np.float32)), mask=True)
 
         return X, y
 
