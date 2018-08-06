dca4909afda097d295502cfdc3555b340c2f1f41
mechaman <julien.hoachuck@gmail.com>
2018-08-06 09:01:06 +0000

    Updated train.py & data_generator.py, Added generation of patient protocol meta data

--- data_generator.py
+++ data_generator.py
@@ -1,15 +1,13 @@
+import keras
 import numpy as np
 import nibabel as nib
-import keras
+from pathlib import Path
 
-from data_loader import *
 
-import numpy as np
-import keras
 
 class DataGenerator(keras.utils.Sequence):
     'Generates data for Keras'
-    def __init__(self, examples, labels, batch_size=1, dim=(256,256), n_channels=1,
+    def __init__(self, examples, labels, batch_size=1, dim=(256,256,256), n_channels=1,
                  n_classes=1, shuffle=True):
         'Initialization'
         self.dim = dim
@@ -18,9 +16,14 @@ class DataGenerator(keras.utils.Sequence):
         self.list_IDs = examples
         self.n_channels = n_channels
         self.shuffle = shuffle
+        # Add an index var. to keep track of location
+        self.index = 0
         self.on_epoch_end()
-
-
+    '''
+    def next(self):
+        'Get the next element in the generator'
+        return self.__getitem__(self.index)
+    '''
     def __len__(self):
         'Denotes the number of batches per epoch'
         return int(np.floor(len(self.list_IDs) / self.batch_size))
@@ -29,14 +32,15 @@ class DataGenerator(keras.utils.Sequence):
         'Generate one batch of data'
         # Generate indexes of the batch
         indexes = self.indexes[index*self.batch_size:(index+1)*self.batch_size]
-
+        #print(len(indexes))
         # Find list of IDs
         list_IDs_temp = [self.list_IDs[k] for k in indexes]
         list_ys_temp = [self.labels[k] for k in indexes]
-        
+        #print(len(list_ys_temp))        
         # Generate data
         X, y = self.__data_generation(list_IDs_temp, list_ys_temp)
-
+        # Update index
+        self.index+=1
         return X, y
 
     def on_epoch_end(self):
@@ -51,36 +55,50 @@ class DataGenerator(keras.utils.Sequence):
         min_val = np.min(x)
         norm_x = (x-min_val)/(max_val - min_val + 1e-7)
         return norm_x
-    
-    def padImage(self, img, mask=False):
-        x_dim = img.shape[0]
-        y_dim = img.shape[1]
-        if x_dim == y_dim:
-            return img
-        else:
-            pad_w = x_dim - y_dim
-            if mask:
-                return np.pad(img, [(0,0), (pad_w//2,pad_w//2), (0,0)],
-                              mode='constant',
-                              constant_values=(1))
-            else:
-                return np.pad(img, [(0,0), (pad_w//2,pad_w//2), (0,0)],
-                              mode='constant')
-                
-                
 
+    def padImage(self, img, mask=False):
+        '''
+        Input -> img : numpy array, mask : Boolean
+        ''' 
+        img_c = img.copy()
+        # Get max dim
+        dims = img.shape
+        max_dim = max(dims)
+        # Padding Args.
+        pad_value = 0
+        pad_arr = [(0,0), (0,0), (0,0)]
+        # Check if mask or input to pad w. 1 or 0
+        if mask:
+            pad_value = 1
+        # Check if other dims eq. largest dim
+        for idx, dim in enumerate(dims):
+            if dim != max_dim:
+                pad_w = max_dim - dim
+                pad_arr[idx] = (pad_w//2, pad_w//2)
+                img_c = np.pad(img, pad_arr,
+                                mode='constant', constant_values=(pad_value))
+
+        return img_c
+
+                
     def __data_generation(self, list_IDs_temp, list_ys_temp):
-        'Generates data containing batch_size samples' # X : (n_samples, *dim, n_channels)
+        'Generates data containing batch_size samples'
         # Initialization
-        X = np.empty((self.batch_size, *self.dim, self.n_channels))
-        y = np.empty((self.batch_size, *self.dim, self.n_channels))
+        X = np.empty((self.batch_size, *self.dim))
+        y = np.empty((self.batch_size, *self.dim))
 
         # Generate data
         for i, ID in enumerate(list_IDs_temp):
+            # @TODO remove when file name consistent
+            ID = ID.replace('.gz', '')
+            # Check if file exists, continue if not
+            if not Path(ID).is_file():
+                print(ID, ' is missing.') 
+                continue
             # Store sample
-            X[i, ] = self.padImage(self.normalizeImg((nib.load(ID).get_data().astype(np.float32))[:, :, np.newaxis]))
+            X[i, ] = self.padImage(self.normalizeImg(nib.load(ID).get_data().astype(np.float32)))
             # Store sample segmentation
-            y[i, ]  = self.padImage((nib.load(list_ys_temp[i]).get_data().astype(np.float32))[:, :, np.newaxis], mask=True)
+            y[i, ]  = self.padImage((nib.load(list_ys_temp[i]).get_data().astype(np.float32)), mask=True)
 
         return X, y
 
--- train.py
+++ train.py
@@ -6,174 +6,323 @@ from keras.utils.training_utils import multi_gpu_model
 from keras.callbacks import ModelCheckpoint, Callback, CSVLogger
 from keras.utils.training_utils import multi_gpu_model
 
+from utils import *
+from metrics import *
 from unet import myUnet
-from data_loader import *
 from data_generator import DataGenerator
 
+def get_2dUnet(dim=(256, 256), slice_type='side', model_fp=''):
+    '''
+    Input -> slice : str, dim : (int, int)
+    Instantiate 2d unet
+    '''
+    # Construct if model_fp not specified
+    if model_fp == '':
+        unet = myUnet(img_rows=dim[0], img_cols=dim[1])
+        model = unet.get_unet()
+    else:
+        model = load_model(model_fp)
+        print('Loaded existing model @ :', model_fp)
+    return model
+
+def save_2dUnet(model, model_fp='', slice_type='side'):
+    '''
+    Save model.
+    '''
+
+    if model_fp == '':
+        # Construct model & weight fp(s) 
+        model_prefix = '2dunet_' + slice_type
+        model_fp = ('./models/' + model_prefix + '.hdf5')
+    model.save(model_fp)
+    return True
+
+def get_slice_batch(data, slice_type, s_idx, f_idx):
+    '''
+    Return a batch of slices for a particular side
+    '''
+    if slice_type == 'top':
+        return data[s_idx:f_idx, :, :]
+    elif slice_type == 'back':
+        return data[:, s_idx:f_idx, :]
+    elif slice_type == 'side':
+        return data[:, :, s_idx:f_idx]
+
+def flip_index(data, slice_type):
+    '''
+    Flips the index so that the slices are indexable by
+    the keras model for training.
+    '''
+    if slice_type == 'top':
+        return data[:, :, :, np.newaxis]
+    elif slice_type == 'back':
+        return np.swapaxes(data, 0, 1)[:,:,:,np.newaxis]
+    elif slice_type == 'side':
+        return np.swapaxes(np.swapaxes(data,0,2), 1,2)[:,:,:,np.newaxis]
+
+def predict_vol(model, data, batch_size, slice_type):
+    '''
+    Predict on the side data.
+    '''
+    # Set input & output masks
+    input_img = (data[0])[0,:,:,:]
+    # Prepare aux. vars. for iter. train on batch
+    T, B, S = input_img.shape
+    # Number of batches is invariant to dim.
+    num_batches = int(np.ceil(S/batch_size))
+    # Batch of side slices
+    side_slice_batch_x = None
+    # Create list of predicted batches
+    predicted_batch = []
+    # Train on each batch
+    for batch_num in range(0, num_batches):
+        s_idx = batch_num*batch_size
+        # Check if last batch
+        if batch_num == (num_batches-1):
+            f_idx = None
+        else:
+            f_idx = s_idx + batch_size
+            #print(s_idx, f_idx)
+        side_slice_batch_x = get_slice_batch(input_img,
+                                            slice_type,
+                                            s_idx, f_idx)
+        # Flip axes to make index to iterate over first 
+        side_slice_batch_x = flip_index(side_slice_batch_x, slice_type = slice_type) 
+        predicted_batch.extend(model.predict_on_batch(side_slice_batch_x))
+    
+    predicted_vol = np.array(predicted_batch)
+    # Compute the average loss and dice scores
+    return predicted_vol
 
 
-def train_zhi_unet(slice_type='side', dim = (256,256), epochs=2):        
-    print('-------- Instantiating Zhi Unet for ', slice_type, dim, '---------------')
-    # Model prefix string for organized persistance
-    model_prefix = 'zhi_unet_' + slice_type
-    train_dir = ('./slice_data_' + slice_type)
-    weights_fp = ('./weights/' + model_prefix + '.hdf5')
-    # Loading Data
+def train_2dUnet(model, data, batch_size, slice_type):
+    '''
+    Train on the side data.
+    '''
+    # Set input & output masks
+    input_img = (data[0])[0,:,:,:]
+    output_mask = (data[1])[0,:,:,:]
+    # Prepare aux. vars. for iter. train on batch
+    T, B, S = input_img.shape
+    # Number of batches is invariant to dim.
+    num_batches = int(np.ceil(S/batch_size))
+    # Batch of side slices
+    side_slice_batch_x = None
+    side_slice_batch_y = None
+    # Loss & Dice array
+    loss = []
+    dice = []
+    # Train on each batch
+    for batch_num in range(0, num_batches):
+        s_idx = batch_num*batch_size
+        # Check if last batch
+        if batch_num == (num_batches-1):
+            f_idx = None 
+        else:
+            f_idx = s_idx + batch_size
+            #print(s_idx, f_idx) 
+        side_slice_batch_x = get_slice_batch(input_img,
+                                            slice_type,
+                                            s_idx, f_idx)
+        side_slice_batch_y= get_slice_batch(input_img,
+                                            slice_type,
+                                            s_idx, f_idx)
+
+        # Flip axes to make index to iterate over first 
+        side_slice_batch_x = flip_index(side_slice_batch_x, slice_type=slice_type)
+        side_slice_batch_y = flip_index(side_slice_batch_y, slice_type=slice_type)
+        #print(side_slice_batch_x.shape, side_slice_batch_y.shape)
+        # Compute and store loss & dice
+        metrics = model.train_on_batch(side_slice_batch_x, side_slice_batch_y)
+        loss.append(metrics[0])
+        dice.append(metrics[1])
+    
+    # Compute the average loss and dice scores
+    avg_loss = np.mean(loss)
+    avg_dice = np.mean(dice)
+    return avg_loss, avg_dice
+
+
+def predict_3d_mask(model_arr, data, batch_size):
+    '''
+    Generate the 3D Mask to compute dice coef. against
+    '''
+    # Predict Side Vol.
+    side_vol = predict_vol(model_arr[0],
+                                data, batch_size,
+                                slice_type='side')
+    #print(side_vol.shape)
+    # Predict Back Vol.
+    back_vol = predict_vol(model_arr[1],
+                                data, batch_size,
+                                slice_type='back')
+    #print(back_vol.shape)
+    # Predict Top Vol.
+    top_vol = predict_vol(model_arr[2],
+                                data, batch_size,
+                                slice_type='top')
+    #print(top_vol.shape) 
+    # Compute avg. voxels across top, side, and back vols
+    avg_vol = np.mean([top_vol, back_vol, side_vol], axis=0)
+    
+    return avg_vol 
+
+def save_stats(loss, dice_2d, dice_3d_train, dice_3d_val):
+    '''
+    Save collected stats from training and validation
+    '''
+    # Save loss during training
+    loss_np = np.array(loss)
+    np.savetxt('./logs/loss.csv', loss_np, delimiter=',')
+    # Save dice_2d during training
+    dice_2d_np = np.array(dice_2d)
+    np.savetxt('./logs/dice_2d.csv', dice_2d_np, delimiter=',')
+    # Save dice_3d_train during training
+    dice_3d_train_np = np.array(dice_3d_train)
+    np.savetxt('./logs/dice_3d_train.csv', dice_3d_train_np, delimiter=',')
+    # save dice_3d_val during training
+    dice_3d_val_np = np.array(dice_3d_val)
+    np.savetxt('./logs/dice_3d_val.csv', dice_3d_val_np, delimiter=',')
+
+    return True
+
+def test_2dUnet_ensemble(dim=(256,256,256)):
+    sides  = 3
+    slice_types = ['back', 'top', 'side']
+    dim = (256, 256)
+    batch_size = 16
+    ## Load Data 
     print('Loading Data...')
+    train_dir = './data'
     partition={}
     (partition['x_train'],
     partition['y_train'],
     partition['x_val'],
     partition['y_val'],
     partition['x_test'],
-    partition['y_test'])  = load_data(train_dir, split=(80, 10, 10))
-    
-    # Save x_val to text file
-    #x_val_fn = ('./val_logs/val_' + slice_type + '.txt')
-    #np.savetxt(x_val_fn, np.array(partition['x_val']), delimiter=',', fmt="%s")
-    
-    # Parameters for input data
-    params1 = {'dim': dim,
-              'batch_size': 32,
-              'n_channels': 1,
-              'shuffle': True}
-    
-    params2 = {'dim': dim,
-          'batch_size': 16,
-          'n_channels': 1,
-          'shuffle': False}
-    training_generator = DataGenerator(partition['x_train'], partition['y_train'], **params1)
-    validation_generator = DataGenerator(partition['x_val'], partition['y_val'], **params2)
-    print('-------------' , len(training_generator))
-    print('-------------',  len(validation_generator))
-    print('Loaded Data...')
+    partition['y_test'])  = load_partitioned_data(train_dir, split=(100, 0, 0))
+    # Define Parameters for generators
+    params1 = { 'dim': (256, 256, 256),
+                'batch_size': 1,
+                'n_channels': 1,
+                'shuffle': True}
+    ## Instantiate Models
+    print("Instantiate Unet Ensemble")
+    # Create model array 
+    model_arr = []
+    # Only if models already exist 
+    model_fp_arr = ['./models/2dunet_back.hdf5', './models/2dunet_top.hdf5', './models/2dunet_side.hdf5']
+    for i in range(3):
+        model_arr.append(get_2dUnet(dim=(256,256), slice_type=slice_types[i], model_fp=model_fp_arr[i]))
+    # Instantiate Test Generator
+    test_generator = DataGenerator(partition['x_test'], partition['y_test'], **params1)
+    # Iterator through test images 
+    for img in test_generator:
+        #@TODO Generator won't stop, need this statement for now. Fix
+        if test_generator.index > len(test_generator):
+            break
+        print('Validation on img number : ', test_generator.index)
+        mask_vol = predict_3d_mask(model_arr, img, batch_size)
+        #Ground Truth Shape:  (1, 256, 256, 256) Predicted Shape:  (256, 256, 256, 1)
+        dice_3d_val.append(dice_coef(img[1][0,:,:,:], mask_vol[:,:,:,0], threshold=0.59))
+    # Print mean dice score
+    print(np.mean(dice_3d_val))
 
-    # Initialize UNet
-    print("Instantiate UNET")
-    unet = myUnet(img_rows=dim[0], img_cols=dim[1])
-    model = unet.get_unet_zhi()
-    
-    # Initialize multi_model
-    #model.load_weights(weights_fp)
-    #multi_model = multi_gpu_model(model, gpus=2)
-    #multi_model.compile(optimizer = Adam(lr = 1e-4), loss = 'binary_crossentropy', metrics = ['accuracy','mse'])
-    model_fp = ('./models/' + model_prefix + '.hdf5')
-    #model = load_model(model_fp)
-    model_checkpoint = ModelCheckpoint(model_fp, monitor='loss',verbose=1,
-                                       save_best_only=True)
-    #print(multi_model.summary())
-
-    # Train UNet
-    print('Fitting Model...')
-    log_fp = ('./logs/' + model_prefix + '3.csv')
-    csv_logger = CSVLogger(log_fp, append=True, separator=';')
-    model.fit_generator(generator=training_generator,
-            validation_data=validation_generator,
-            validation_steps = 1,
-            epochs=epochs,
-            verbose=1,
-            callbacks =[model_checkpoint, csv_logger],
-            use_multiprocessing=True,
-            workers=6)
-    
-    # Save weights
-    #print(multi_model.summary())
-    #model = multi_model.get_layer('model_1')
-    model.save_weights(weights_fp)
-                                  
-
-def predict(slice_type='side'):
-	# @DEBUG MAKE SURE WORKS!!!!
-	# Model prefix string for organized persistance
-    model_prefix = 'zhi_unet_' + slice_type
-    
-    # Loading Data
+def train_2dUnet_ensemble(dim = (256,256,256), epochs=2):        
+    sides  = 3
+    slice_types = ['back', 'top', 'side']
+    dim = (256, 256)
+    batch_size = 16
+    ## Load Data 
     print('Loading Data...')
-    partition={}
-
-    (_,
-     _,
-     _,
-     _,
-     x_test,
-     y_test)  = load_data('slice_data_side_test', split=(0.0, 0.0, 100))
-
-    # Parameters for input data
-    params = {'dim': (256,256),
-                  'batch_size': 1,
-                  'n_channels': 1,
-                  'shuffle': True}
-    test_generator = DataGenerator(x_test, y_test, **params)
-    print('Loaded Data...')
-    
-    # Initialize UNet
-    print("Instantiate UNET")
-    unet = myUnet()
-    model = unet.get_unet_zhi()
-    model_fp = ('./models/' + model_prefix + '.hdf5')
-    model_checkpoint = ModelCheckpoint(model_fp, monitor='loss',verbose=1,
-                                           save_best_only=True)
-    weight_fp = ('./weights/' + model_prefix + '.hdf5')
-    model.load_weights(weights_fp)
-
-    # Predict using UNet
-    print("Predicting via UNet")
-    unet.predict(model, test_generator, y_test, slice_type)
-
-def train_basic_unet(slice_type = 'side'):
-    # Model prefix string for organized persistance
-    model_prefix = 'basic_unet_' + slice_type
-    
-    # Loading Data
-    print('Loading Data...')
+    train_dir = './data'
     partition={}
     (partition['x_train'],
-     partition['y_train'],
-     partition['x_val'],
-     partition['y_val'],
-     partition['x_test'],
-     partition['y_test'])  = load_data('slice_data_side', split=(0.005, 0.005, 99.99))
+    partition['y_train'],
+    partition['x_val'],
+    partition['y_val'],
+    partition['x_test'],
+    partition['y_test'])  = load_partitioned_data(train_dir, split=(80, 10, 10))
+    # Define Parameters for generators
+    params1 = { 'dim': (256, 256, 256),
+                'batch_size': 1,
+                'n_channels': 1,
+                'shuffle': True}
+    ## Instantiate Models
+    print("Instantiate Unet Ensemble")
+    # Create model array 
+    model_arr = []
+    # Only if models already exist 
+    model_fp_arr = ['./models/2dunet_back.hdf5', './models/2dunet_top.hdf5', './models/2dunet_side.hdf5']
+    for i in range(3):
+        model_arr.append(get_2dUnet(dim=(256,256), slice_type=slice_types[i], model_fp=model_fp_arr[i]))
+    
+    # Iterate through Epochs 
+    for i in range(0, epochs): 
+        print('Epoch : ', i)
+        ## Train Models
+        loss = []
+        dice_2d = []
+        dice_3d_train = []
+        # Create a list of loss/dice for each model
+        for i in range(0,len(model_arr)):
+            loss.append([])
+            dice_2d.append([])
+        
+        # Instantiate dataset generators 
+        training_generator = DataGenerator(partition['x_train'], partition['y_train'], **params1)
+        validation_generator = DataGenerator(partition['x_val'], partition['y_val'], **params1)
+    
+        ## Training Loop 
+        print('Number of images to train on : ', len(training_generator))  
+        for img in training_generator:
+            #@TODO Generator won't stop, need this statement for now. Fix
+            if training_generator.index > len(training_generator):
+                break
+            print('Training on img number : ', training_generator.index) 
+            ## Train 2D Models & Get Metrics 
+            # Model 0 : Back Slice 
+            img_loss, img_dice = train_2dUnet(model_arr[0], img, batch_size, slice_type='back')
+            loss[0].append(img_loss)
+            dice_2d[0].append(img_dice)
+            # Model 2 : Side Slice 
+            img_loss, img_dice = train_2dUnet(model_arr[1], img, batch_size, slice_type='top')
+            loss[1].append(img_loss)
+            dice_2d[1].append(img_dice)
+            # Model 2 : Side Slice 
+            img_loss, img_dice = train_2dUnet(model_arr[2], img, batch_size, slice_type='side')
+            #print(img_loss, img_dice)
+            loss[2].append(img_loss)
+            dice_2d[2].append(img_dice)
+            ## Get 3D Dice
+            # Predict 3D Mask
+            #mask_vol = predict_3d_mask(model_arr, img, batch_size)
+            # Compute dice coef. 
+            #Ground Truth Shape:  (1, 256, 256, 256) Predicted Shape:  (256, 256, 256, 1)
+            #dice_3d_train.append(dice_coef(img[1][0,:,:,:], mask_vol[:,:,:,0], threshold=0.59))
+        print(dice_3d_train[-10:])
+         
+        ## Validation Loop
+        dice_3d_val = []
+        '''print('Number of images to validate on : ', len(validation_generator))
+        for img in validation_generator:
+            #@TODO Generator won't stop, need this statement for now. Fix
+            if validation_generator.index > len(validation_generator):
+                break
+            print('Validation on img number : ', validation_generator.index)
+            mask_vol = predict_3d_mask(model_arr, img, batch_size)
+            #Ground Truth Shape:  (1, 256, 256, 256) Predicted Shape:  (256, 256, 256, 1)
+            dice_3d_val.append(dice_coef(img[1][0,:,:,:], mask_vol[:,:,:,0], threshold=0.59))
+        print(dice_3d_val[-10:])
+        '''
+        # Save stats  
+        save_stats(loss, dice_2d, dice_3d_train, dice_3d_val)
 
-    # Parameters for input data
-    params = {'dim': (256,256),
-                  'batch_size': 1,
-                  'n_channels': 1,
-                  'shuffle': False}
-    training_generator = DataGenerator(partition['x_train'], partition['y_train'], **params)
-    validation_generator = DataGenerator(partition['x_val'], partition['y_val'], **params)
-    print('Loaded Data...')
-    
-    # Initialize UNet
-    print("Instantiate UNET")
-    unet = myUnet()
-    model = unet.get_unet_basic()
-    model_fp = ('./models/' + model_prefix + '.hdf5')
-    model = model.load
-    model_checkpoint = ModelCheckpoint(model_fp, monitor='loss',verbose=1,
-                                           save_best_only=True)
-    print(model.summary())
-    
-    # Train UNet
-    print('Fitting Model...')
-    log_fp = ('./logs/' + model_prefix + '.csv')
-    csv_logger = CSVLogger(log_fp, append=True, separator=';')
-    model.fit_generator(generator=training_generator,
-                validation_data=validation_generator,
-                validation_steps = 1,
-                epochs=2,
-                verbose=1,
-                callbacks =[model_checkpoint, csv_logger],
-                use_multiprocessing=True,
-                workers=6)
-    
-    # Save weights
-    weights_fp = ('./weights/' + model_prefix + '.hdf5')
-    model.save_weights(weights_fp)
-    
+        ## Save the models
+        for idx, model in enumerate(model_arr):
+            save_2dUnet(model, slice_type=slice_types[idx])
+        
 
 if __name__ == '__main__':
-    #train_basic_unet(slice_type='side')
-    #train_zhi_unet(slice_type='side', dim = (256, 256), epochs=1)
-    train_zhi_unet(slice_type='top', dim = (256, 256), epochs=3)
-    #train_zhi_unet(slice_type='back', dim=(256, 256), epochs=1)
+    train_2dUnet_ensemble(dim=(256,256,256), epochs=1) 
     
